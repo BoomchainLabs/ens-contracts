ef540abd65c4d6d8e453a6ffcbc0862d413593e3
tate <tate@ens.domains>
2025-12-08 12:18:01 +1100

    v1.6.1

--- contracts/resolvers/profiles/ICompositeResolver.sol
+++ contracts/resolvers/profiles/ICompositeResolver.sol
@@ -8,15 +8,17 @@ import {IExtendedResolver} from "./IExtendedResolver.sol";
 interface ICompositeResolver is IExtendedResolver {
     /// @notice Fetch the underlying resolver for `name`.
     ///         Callers should enable EIP-3668.
-	///
-	/// * If `offchain`, additional information is necessary to locate `resolver`.
-	/// * If `resolver` is null, `offchain` is irrelevant.
+    ///
+    /// * If `offchain`, additional information is necessary to locate `resolver`.
+    /// * If `resolver` is null, `offchain` is irrelevant.
     ///
     /// @param name The DNS-encoded name.
     ///
     /// @return resolver The underlying resolver address.
     /// @return offchain `true` if `resolver` is offchain.
-    function getResolver(bytes memory name) external view returns (address resolver, bool offchain);
+    function getResolver(
+        bytes memory name
+    ) external view returns (address resolver, bool offchain);
 
     /// @notice Determine if resolving `name` requires offchain data.
     ///
